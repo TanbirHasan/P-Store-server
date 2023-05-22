e4b3e27188a2e47bb24bda791080da2b0a29a586
sajid1545 <clashking1545@gmail.com>
2023-05-22 12:18:01 +0600

    initial setup done

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
+const PORT = process.env.PORT || 8000;
 
-const mongoose = require("mongoose");
-
-
-
+const mongoose = require('mongoose');
+const connectDB = require('./DB/connectDB');
 
 // middlewares
 
 app.use(express.json());
 app.use(cors());
 
-
-
-
-
-app.get("/", (req, res) => {
-  res.send("Route is working");
+app.get('/', (req, res) => {
+	res.send('Route is working');
 });
 
 
+// Start Server and Connect to DB
 
-
+const start = async () => {
+	try {
+		await connectDB('mongodb://127.0.0.1:27017/test');
+		app.listen(PORT, () => {
+			console.log(`App is running on port ${PORT}`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+start();
 
 module.exports = app;
