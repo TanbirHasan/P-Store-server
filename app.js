1d376445bc198a2fe2d86ca24fb9b0c6e4cbb042
TanbirHasan <mdtanbir576@gmail.com>
2023-05-29 20:58:23 +0600

    login auth added

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ const PORT = process.env.PORT || 8000;
 const mongoose = require('mongoose');
 const connectDB = require('./DB/connectDB');
 const dotenv =  require("dotenv").config()
+const registerRoutes = require('./routes/register')
+const loginRoutes = require('./routes/login')
 
 // middlewares
 
 app.use(express.json());
 app.use(cors());
+
+
+// routes
+
+
+app.use('/api/v1/register',registerRoutes );
+app.use('/api/v1/login', loginRoutes);
 
 app.get('/', (req, res) => {
 	res.send('Route is working');
---
+++ controllers/loginController.js
@@ -0,0 +1,52 @@
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const user = require('../models/user');
+
+
+const handleLogin = async (req, res) => {
+    const { email, password } = req.body;
+   // console.log(user,pwd)
+    if (!email || !password) return res.status(400).json({ 'message': 'Username and password are required.' });
+
+    const foundUser = await user.findOne({ email }).exec();
+    console.log("Found User",foundUser)
+    if (!foundUser) return res.sendStatus(401); 
+
+    const match = await bcrypt.compare(password, foundUser.password);
+    if (match) {
+        const roles = Object.values(foundUser.roles).filter(Boolean);
+        // create JWTs
+        const accessToken = jwt.sign(
+            {
+                "UserInfo": {
+                    "email": foundUser.email,
+                    "roles": roles
+                }
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '5m' }
+        );
+        const refreshToken = jwt.sign(
+            { "email": foundUser.email },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+        // Saving refreshToken with current user
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result);
+        console.log(roles);
+
+        // Creates Secure Cookie with refresh token
+        res.cookie('jwt', refreshToken, { httpOnly: true,  sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+
+        // Send authorization roles and access token to user
+        res.json({ roles, accessToken });
+
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = { handleLogin };
