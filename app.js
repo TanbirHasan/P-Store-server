3cb4873810d30ecb8d9bae365f6a8607d8b5f1b4
TanbirHasan <mdtanbir576@gmail.com>
2023-06-02 11:22:40 +0600

    added login api

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const mongoose = require('mongoose');
 const connectDB = require('./DB/connectDB');
 const dotenv =  require("dotenv").config()
 const registerRoutes = require('./routes/register')
-const loginRoutes = require('./routes/login')
+const loginRoutes = require('./routes/login');
+const corsOptions = require('./config/corsOptions');
+const credentials = require('./middelwares/credentials');
+
 
 // middlewares
 
 app.use(express.json());
-app.use(cors());
+app.use(credentials);
+app.use(cors(corsOptions));
 
 
 // routes
